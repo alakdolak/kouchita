a5a0d1a2acad8edebc79500e8bef1149cc082cad
navidfaghih <navidfaghih@aut.ac.ir>
2019-09-23 16:48:03 +0330

    shareBox styles clean up

--- resources/views/layouts/shareBox.blade.php
+++ resources/views/layouts/shareBox.blade.php
@@ -4,25 +4,24 @@
 
     <a target="_blank" style="" class="link" {{($config->facebookNoFollow) ? 'rel="nofollow"' : ''}}
     href="https://www.facebook.com/sharer/sharer.php?u={{Request::url()}}">
-        <div style="background-image: url('{{URL::asset('images') . '/share.png'}}');width: 30px;height: 30px;background-size: 30px;background-position:  0 0;background-repeat:  no-repeat;display: inline-block;"></div>
+        <div style=""></div>
     </a>
-    <a target="_blank" style="cursor: pointer; float: left;margin-left: 5px;" class="link" {{($config->telegramNoFollow) ? 'rel="nofollow"' : ''}}
+    <a target="_blank" class="link" {{($config->telegramNoFollow) ? 'rel="nofollow"' : ''}}
     href="https://telegram.me/share/url?url={{Request::url()}}">
-        <div style="background-image: url('{{URL::asset('images') . '/share.png'}}');width: 30px;height: 30px;background-size: 30px;background-position:  0 -60px;background-repeat:  no-repeat;display: inline-block;border-radius: 4px;"></div>
+        <div style=""></div>
     </a>
-    <a target="_blank" style="cursor: pointer; float: left;margin-left: 5px;" class="link" {{($config->googlePlusNoFollow) ? 'rel="nofollow"' : ''}}
+    <a target="_blank" class="link" {{($config->googlePlusNoFollow) ? 'rel="nofollow"' : ''}}
     href="https://plus.google.com/share?url={{str_replace('%20', '', Request::url())}}">
-        <div style="background-image: url('{{URL::asset('images') . '/share.png'}}');width: 30px;height: 30px;background-size: 30px;background-position:  0 -30px;background-repeat:  no-repeat;display: inline-block;"></div>
+        <div></div>
     </a>
-    <a target="_blank" style="cursor: pointer; float: left;margin-left: 5px;" class="link" {{($config->twitterNoFollow) ? 'rel="nofollow"' : ''}} href="https://twitter.com/home?status={{Request::url()}}">
-        <div style="background-image: url('{{URL::asset('images') . '/share.png'}}');width: 30px;height: 30px;background-size: 30px;background-position:  0 -90px;background-repeat:  no-repeat;display: inline-block;"></div>
+    <a target="_blank" class="link" {{($config->twitterNoFollow) ? 'rel="nofollow"' : ''}} href="https://twitter.com/home?status={{Request::url()}}">
+        <div></div>
     </a>
-    <span style="cursor: pointer; float: right;margin-left: 5px;line-height: 28px;font-size: 14px;">اشتراک گذاری</span>
+    <span>اشتراک گذاری</span>
 </span>
 <div class="header heading fr">
-    <a style="cursor: pointer; float: left;" class="link" onclick="startHelp()">
-        <div style="background-size: 28px;background-position:  0 -29px;width: 28px;height:  28px;background-image: url('{{URL::asset('images') . '/help_share.png'}}');background-repeat:  no-repeat;"></div>
+    <a class="link" onclick="startHelp()">
+        <div></div>
     </a>
-    <div id="share_pic"
-         style="cursor: pointer;background-size: 28px;background-position:  0 0;width: 28px;height:  28px;background-image: url('{{URL::asset('images') . '/help_share.png'}}');background-repeat:  no-repeat;float: right;margin-left: 5px;"></div>
+    <div id="share_pic"></div>
 </div>
