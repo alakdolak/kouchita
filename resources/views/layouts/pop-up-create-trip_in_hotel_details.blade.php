8651c25705179c05dab91c626735263b820a8bbe
navidfaghih <navidfaghih@aut.ac.ir>
2019-09-25 11:46:57 +0330

    pop up create trip in hotel details styles clean up

--- resources/views/layouts/pop-up-create-trip_in_hotel_details.blade.php
+++ resources/views/layouts/pop-up-create-trip_in_hotel_details.blade.php
@@ -89,10 +89,11 @@
 <script async src="{{URL::asset("js/bootstrap-datepicker.js")}}"></script>
 
 <link rel="stylesheet" href="{{URL::asset('css/theme2/bootstrap-datepicker.css?v=1')}}">
+<link rel='stylesheet' type='text/css' media='screen, print' href='{{URL::asset('css/theme2/popUpCreateTripInHotelDetails.css')}}'/>
 
-<div id="pop-up-create-trip" class="ui_modal_card saves-settings-modal create-trip-modal is-active" style="z-index: 100000000001; visibility: hidden">
+<div id="pop-up-create-trip" class="ui_modal_card saves-settings-modal create-trip-modal is-active">
     <div class="modal-background" onclick=""></div>
-    <div class="modal-card" style="direction: rtl">
+    <div class="modal-card rtl">
         <div class="ui_close_x" onclick="closePopUp()"></div>
         <div class="modal-card-head">
             <p class="modal-card-title">ایجاد سفر </p>
@@ -100,7 +101,7 @@
         <div class="modal-card-body">
             <div id="trip-title-input-region">
                 <div>
-                    <label class="label" style="color: #2c2c2c; font-size: 100% !important;">نام سفر</label>
+                    <label class="label">نام سفر</label>
                     <div class="control trip-title-control">
                         <input class="trip-title ui_input_text" id="tripName" onkeyup="checkEmpty()" type="text" maxlength="50" placeholder="حداکثر 50 کاراکتر" value="">
                     </div>
@@ -113,30 +114,30 @@
     </div>
 </div>
 
-<div id="date-pop-up" class="ui_modal_card saves-settings-modal create-trip-modal is-active" style="z-index: 10000000001;  visibility: hidden;">
+<div id="date-pop-up" class="ui_modal_card saves-settings-modal create-trip-modal is-active">
     <div class="modal-background" onclick=""></div>
     <div class="modal-card">
         <div class="ui_close_x" onclick="closePopUp()"></div>
         <div class="modal-card-head">
-            <p style="direction: rtl" class="modal-card-title">افزودن تاریخ به سفر</p>
+            <p class="modal-card-title rtl">افزودن تاریخ به سفر</p>
         </div>
-        <div class="modal-card-body" style="direction: rtl">
+        <div class="modal-card-body rtl">
             <div class="control add-dates-cta-text">
                 <p></p>
             </div>
             <div class="trip-dates ui_columns">
                 <div class="ui_column">
                     <div id="date_btn_start_edit">تاریخ شروع</div>
-                    <label style="position: relative; margin:6px; width: 100%; height: 30px; border: 1px solid #e5e5e5; border-radius: 2px; box-shadow: 0 7px 12px -7px #e5e5e5 inset;">
-                        <span class="ui_icon calendar" style="color: #30b4a6 !important; font-size: 20px; line-height: 32px; position: absolute; right: 7px;"></span>
-                        <input id="date_input_start" placeholder="روز/ماه/سال" required readonly style="padding: 7px; position: absolute; top: 1px; right: 35px; border: none; background: transparent;" type="text">
+                    <label id="date_input_start_label">
+                        <span class="ui_icon calendar"></span>
+                        <input id="date_input_start" placeholder="روز/ماه/سال" required readonly type="text">
                     </label>
                 </div>
                 <div class="ui_column">
                     <div id="date_btn_end_edit">تاریخ اتمام</div>
-                    <label style="position: relative; margin:6px; width: 100%; height: 30px; border: 1px solid #e5e5e5; border-radius: 2px; box-shadow: 0 7px 12px -7px #e5e5e5 inset;">
-                        <span class="ui_icon calendar" style="color: #30b4a6 !important; font-size: 20px; line-height: 32px; position: absolute; right: 7px;"></span>
-                        <input id="date_input_end" placeholder="روز/ماه/سال" required readonly style="padding: 7px; position: absolute; top: 1px; right: 35px; border: none; background: transparent;" type="text">
+                    <label id="date_input_end_label">
+                        <span class="ui_icon calendar"></span>
+                        <input id="date_input_end" placeholder="روز/ماه/سال" required readonly type="text">
                     </label>
                 </div>
             </div>
--- resources/views/layouts/shareBox.blade.php
+++ resources/views/layouts/shareBox.blade.php
@@ -1,29 +1,27 @@
+<link rel='stylesheet' type='text/css' media='screen, print' href='{{URL::asset('css/theme2/shareBox.css')}}'/>
 
+<span id="share_box">
 
-<span id="share_box"
-      style="display: none; width: 22%;height: 60px;background: #FFF;position: absolute;left: 7%;z-index: 10;box-shadow: rgb(136, 136, 136) 1px 4px 8px;padding: 10px;">
-
-    <a target="_blank" style="cursor: pointer; float: left;margin-left: 5px;" class="link" {{($config->facebookNoFollow) ? 'rel="nofollow"' : ''}}
+    <a target="_blank" style="" class="link" {{($config->facebookNoFollow) ? 'rel="nofollow"' : ''}}
     href="https://www.facebook.com/sharer/sharer.php?u={{Request::url()}}">
-        <div style="background-image: url('{{URL::asset('images') . '/share.png'}}');width: 30px;height: 30px;background-size: 30px;background-position:  0 0;background-repeat:  no-repeat;display: inline-block;"></div>
+        <div style=""></div>
     </a>
-    <a target="_blank" style="cursor: pointer; float: left;margin-left: 5px;" class="link" {{($config->telegramNoFollow) ? 'rel="nofollow"' : ''}}
+    <a target="_blank" class="link" {{($config->telegramNoFollow) ? 'rel="nofollow"' : ''}}
     href="https://telegram.me/share/url?url={{Request::url()}}">
-        <div style="background-image: url('{{URL::asset('images') . '/share.png'}}');width: 30px;height: 30px;background-size: 30px;background-position:  0 -60px;background-repeat:  no-repeat;display: inline-block;border-radius: 4px;"></div>
+        <div style=""></div>
     </a>
-    <a target="_blank" style="cursor: pointer; float: left;margin-left: 5px;" class="link" {{($config->googlePlusNoFollow) ? 'rel="nofollow"' : ''}}
+    <a target="_blank" class="link" {{($config->googlePlusNoFollow) ? 'rel="nofollow"' : ''}}
     href="https://plus.google.com/share?url={{str_replace('%20', '', Request::url())}}">
-        <div style="background-image: url('{{URL::asset('images') . '/share.png'}}');width: 30px;height: 30px;background-size: 30px;background-position:  0 -30px;background-repeat:  no-repeat;display: inline-block;"></div>
+        <div></div>
     </a>
-    <a target="_blank" style="cursor: pointer; float: left;margin-left: 5px;" class="link" {{($config->twitterNoFollow) ? 'rel="nofollow"' : ''}} href="https://twitter.com/home?status={{Request::url()}}">
-        <div style="background-image: url('{{URL::asset('images') . '/share.png'}}');width: 30px;height: 30px;background-size: 30px;background-position:  0 -90px;background-repeat:  no-repeat;display: inline-block;"></div>
+    <a target="_blank" class="link" {{($config->twitterNoFollow) ? 'rel="nofollow"' : ''}} href="https://twitter.com/home?status={{Request::url()}}">
+        <div></div>
     </a>
-    <span style="cursor: pointer; float: right;margin-left: 5px;line-height: 28px;font-size: 14px;">اشتراک گذاری</span>
+    <span>اشتراک گذاری</span>
 </span>
-<div class="header heading fr">
-    <a style="cursor: pointer; float: left;" class="link" onclick="startHelp()">
-        <div style="background-size: 28px;background-position:  0 -29px;width: 28px;height:  28px;background-image: url('{{URL::asset('images') . '/help_share.png'}}');background-repeat:  no-repeat;"></div>
+<div class="header heading fr" id="lastDivOfShareBox">
+    <a class="link" onclick="startHelp()">
+        <div></div>
     </a>
-    <div id="share_pic"
-         style="cursor: pointer;background-size: 28px;background-position:  0 0;width: 28px;height:  28px;background-image: url('{{URL::asset('images') . '/help_share.png'}}');background-repeat:  no-repeat;float: right;margin-left: 5px;"></div>
+    <div id="share_pic"></div>
 </div>
