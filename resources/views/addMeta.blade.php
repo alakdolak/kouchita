baec314ec919bbd1c66c7882f78ee2c4a37ee7a1
navidfaghih <navidfaghih@aut.ac.ir>
2019-09-28 15:05:55 +0330

    add meta styles final clean up

--- resources/views/addMeta.blade.php
+++ resources/views/addMeta.blade.php
@@ -20,37 +20,42 @@
 
 @section('main')
 
+
+    <link rel="stylesheet" href="{{URL::asset('css/theme2/specific designs/addMeta.css')}}">
+
     <center class="row">
         @if($mode2 == 'select1')
             <div class="col-xs-12">
-                <a href="{{URL('addMeta/kind=adab')}}"><button class="btn btn-primary" style="width: auto">آداب و رسوم</button></a>
+                <a href="{{URL('addMeta/kind=adab')}}"><button class="btn btn-primary width-auto">آداب و رسوم</button></a>
             </div>
             <div class="col-xs-12">
-                <a href="{{URL('addMeta/kind=amaken')}}"><button class="btn btn-primary" style="width: auto">اماکن گردشگری</button></a>
+                <a href="{{URL('addMeta/kind=amaken')}}"><button class="btn btn-primary width-auto">اماکن گردشگری</button></a>
             </div>
             <div class="col-xs-12">
-                <a href="{{URL('addMeta/kind=concert')}}"><button class="btn btn-primary" style="width: auto">کنسرت</button></a>
+                <a href="{{URL('addMeta/kind=concert')}}"><button class="btn btn-primary width-auto">کنسرت</button></a>
             </div>
             <div class="col-xs-12">
-                <a href="{{URL('addMeta/kind=hotel')}}"><button class="btn btn-primary" style="width: auto">هتل</button></a>
+                <a href="{{URL('addMeta/kind=hotel')}}"><button class="btn btn-primary width-auto">هتل</button></a>
             </div>
             <div class="col-xs-12">
-                <a href="{{URL('addMeta/kind=jashnvare')}}"><button class="btn btn-primary" style="width: auto">جشنواره</button></a>
+                <a href="{{URL('addMeta/kind=jashnvare')}}"><button class="btn btn-primary width-auto">جشنواره</button></a>
             </div>
             <div class="col-xs-12">
-                <a href="{{URL('addMeta/kind=majara')}}"><button class="btn btn-primary" style="width: auto">ماجراجویی</button></a>
+                <a href="{{URL('addMeta/kind=majara')}}"><button class="btn btn-primary width-auto">ماجراجویی</button></a>
             </div>
             <div class="col-xs-12">
-                <a href="{{URL('addMeta/kind=restaurant')}}"><button class="btn btn-primary" style="width: auto">رستوران</button></a>
+                <a href="{{URL('addMeta/kind=restaurant')}}"><button class="btn btn-primary width-auto">رستوران</button></a>
             </div>
             <div class="col-xs-12">
-                <a href="{{URL('addMeta/kind=rosoom')}}"><button class="btn btn-primary" style="width: auto">رسوم</button></a>
+                <a href="{{URL('addMeta/kind=rosoom')}}"><button class="btn btn-primary width-auto">رسوم</button></a>
             </div>
 
         @elseif($mode2 == "select2")
             @foreach($target as $itr)
                 <div class="col-xs-12">
-                    <a href="{{URL('addMeta/kind=' . $kind . '/id=' . $itr->id)}}"><button class="btn btn-primary" style="width: auto">{{$itr->name}}</button></a>
+                    <a href="{{URL('addMeta/kind=' . $kind . '/id=' . $itr->id)}}">
+                        <button class="btn btn-primary width-auto">{{$itr->name}}</button>
+                    </a>
                 </div>
             @endforeach
 
@@ -59,10 +64,10 @@
                 {{csrf_field()}}
                 <div class="col-xs-12">
                     <h4>متن مورد نظر خود را وارد نمایید</h4>
-                    <textarea style="width: 60%; height: 400px" name="newMeta" maxlength="1024">{{$meta}}</textarea>
+                    <textarea id="mainTextArea" name="newMeta" maxlength="1024">{{$meta}}</textarea>
                 </div>
                 <div class="col-xs-12">
-                    <input type="submit" class="btn btn-primary" style="width: auto" value="تایید">
+                    <input type="submit" class="btn btn-primary width-auto" value="تایید">
                 </div>
             </form>
         @endif
