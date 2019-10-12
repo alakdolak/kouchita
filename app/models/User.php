4f86f2542b9ed7edf6d653d8e8683caeadfa93c6
kiavash25 <kiavashriddler@gmail.com>
2019-10-12 14:35:49 +0330

    kia

--- app/models/User.php
+++ app/models/User.php
@@ -2,82 +2,111 @@
 
 
 namespace App\models;
+//
+//use Illuminate\Database\Eloquent\Model;
+//
+//use Illuminate\Notifications\Notifiable;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+//
+//
+///**
+// * An Eloquent Model: 'User'
+// *
+// * @property integer $id
+// * @property string $first_name
+// * @property string $last_name
+// * @property string $email
+// * @property string $phone
+// * @property string $username
+// * @property string $password
+// * @property string $invitationCode
+// * @property string $link
+// * @property integer $cityId
+// * @property integer $level
+// * @property boolean $uploadPhoto
+// * @property boolean $status
+// * @method static \Illuminate\Database\Query\Builder|\App\models\User whereUserName($value)
+// * @method static \Illuminate\Database\Query\Builder|\App\models\User whereEmail($value)
+// * @method static \Illuminate\Database\Query\Builder|\App\models\User whereLevel($value)
+// * @method static \Illuminate\Database\Query\Builder|\App\models\User whereInvitationCode($value)
+// * @method static \Illuminate\Database\Query\Builder|\App\models\User wherePhone($value)
+// */
+//
+//class User extends Authenticatable{
+//
+//    use Notifiable;
+//    /**
+//     * The attributes that are mass assignable.
+//     *
+//     * @var array
+//     */
+//
+//    protected $table = 'users';
+//    protected $primaryKey = 'id';
+//    /**
+//     * The attributes excluded from the model's JSON form.
+//     *
+//     * @var array
+//     */
+//
+//    protected $fillable = [
+//        'username', 'password'
+//    ];
+//
+//    protected $hidden = array('password', 'remember_token');
+//
+//    public function getRememberToken()
+//    {
+//        dd('fkdjfd');
+//        return $this->remember_token;
+//    }
+//
+//    public function setRememberToken($value)
+//    {
+//        $this->remember_token = $value;
+//    }
+//
+//    public function getRememberTokenName()
+//    {
+//        return 'remember_token';
+//    }
+//
+//    public function getAuthIdentifier() {
+//        return $this->getKey();
+//    }
+//    public function getAuthPassword() {
+//        return $this->password;
+//    }
+//
+//    public static function whereId($value) {
+//        return User::find($value);
+//    }
+//}
 
-use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
+class User extends Authenticatable
+{
+    use HasApiTokens, Notifiable;
 
-/**
- * An Eloquent Model: 'User'
- *
- * @property integer $id
- * @property string $first_name
- * @property string $last_name
- * @property string $email
- * @property string $phone
- * @property string $username
- * @property string $password
- * @property string $invitationCode
- * @property string $link
- * @property integer $cityId
- * @property integer $level
- * @property boolean $uploadPhoto
- * @property boolean $status
- * @method static \Illuminate\Database\Query\Builder|\App\models\User whereUserName($value)
- * @method static \Illuminate\Database\Query\Builder|\App\models\User whereEmail($value)
- * @method static \Illuminate\Database\Query\Builder|\App\models\User whereLevel($value)
- * @method static \Illuminate\Database\Query\Builder|\App\models\User whereInvitationCode($value)
- * @method static \Illuminate\Database\Query\Builder|\App\models\User wherePhone($value)
- */
-
-class User extends Authenticatable{
-
-    use Notifiable;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
 
-    protected $table = 'users';
-    protected $primaryKey = 'id';
     /**
-     * The attributes excluded from the model's JSON form.
+     * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-
-    protected $fillable = [
-        'username', 'password'
+    protected $hidden = [
+        'password', 'remember_token',
     ];
-
-    protected $hidden = array('password', 'remember_token');
-
-    public function getRememberToken()
-    {
-        return $this->remember_token;
-    }
-
-    public function setRememberToken($value)
-    {
-        $this->remember_token = $value;
-    }
-
-    public function getRememberTokenName()
-    {
-        return 'remember_token';
-    }
-
-    public function getAuthIdentifier() {
-        return $this->getKey();
-    }
-    public function getAuthPassword() {
-        return $this->password;
-    }
-
-    public static function whereId($value) {
-        return User::find($value);
-    }
 }
