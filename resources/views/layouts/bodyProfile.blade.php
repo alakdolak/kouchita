750810d4b83d0746eb7e677e6f7840961d2da07b
kiavash25 <kiavashriddler@gmail.com>
2019-10-16 18:47:54 +0330

    گزارشات - سوالات

--- resources/views/layouts/bodyProfile.blade.php
+++ resources/views/layouts/bodyProfile.blade.php
@@ -325,7 +325,7 @@
                         @if(Auth::user()->level == 1)
                             {{--<a title="Add meta" href="{{route('addMeta')}}">اضافه کردن meta</a>--}}
                             {{--<a title="Activities" href="{{route('activities')}}">فعالیت ها</a>--}}
-                            {{--<a title="Descriptions" href="{{route('descriptions')}}">توضیحات</a>--}}
+                            <a title="Descriptions" href="{{route('descriptions')}}">توضیحات</a>
                             {{--<a title="Places" href="{{route('places')}}">اماکن</a>--}}
 {{--                            <a title="Medals" href="{{route('medals')}}">مدال ها</a>--}}
                             {{--<a title="Levels" href="{{route('levels')}}">سطوح</a>--}}
@@ -336,9 +336,9 @@
                             {{--<a title="tags" href="{{route('tags')}}">تگ ها</a>--}}
 {{--                            <a title="ages" href="{{route('picItems')}}">آیتم تصاویر</a>--}}
                             {{--<a title="ages" href="{{route('opinions')}}">مدیریت نظرات</a>--}}
-                            <a title="ages" href="{{route('questions')}}">سوالات نظرسنجی</a>
+                            {{--<a title="ages" href="{{route('questions')}}">سوالات نظرسنجی</a>--}}
 {{--                            <a title="ages" href="{{route('specialAdvice')}}">پیشنهاد های ویژه</a>--}}
-                            <a title="ages" href="{{route('reports')}}">گزارشات</a>
+                            {{--<a title="ages" href="{{route('reports')}}">گزارشات</a>--}}
                             <a title="ages" href="{{route('goyeshTags')}}">تگ های گویش</a>
                             <a title="ages" href="{{route('ageSentences')}}">توضیحات سن برای بلیط</a>
                         @endif
