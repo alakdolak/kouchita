7e4fa10eefdaa767e0d27445f03c39912f800c96
kiavash25 <kiavashriddler@gmail.com>
2019-12-25 13:17:33 +0330

    show reviews

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -500,7 +500,7 @@ class AjaxController extends Controller {
     public function reviewUploadPic(Request $request)
     {
         $location = __DIR__ . '/../../../../assets';
-
+dd(file_exists($location), file_exists($location .'/'));
         if(!file_exists($location))
             mkdir($location);
 
