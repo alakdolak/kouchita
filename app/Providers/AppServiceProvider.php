a9b69d2dc2f467f88a700e0be9f30f1eb57809d9
kiavash25 <kiavashriddler@gmail.com>
2020-01-08 11:07:30 +0330

    kiavash

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,11 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        dd(str_contains(\Config::get('app.url'), 'localhost'), \Config::get('app.url'));
-        if(!str_contains(\Config::get('app.url'), 'localhost')) {
-            if (str_contains(\Config::get('app.url'), 'http://')) {
-                \URL::forceScheme('https');
-            }
+        if (str_contains(\Config::get('app.url'), 'http://')) {
+            \URL::forceScheme('https');
         }
     }
 
