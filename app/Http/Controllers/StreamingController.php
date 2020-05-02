e8ac72d8eb9cb6e5b3f40e248f613430831a8af5
kiavash25 <kiavashriddler@gmail.com>
2020-05-03 03:20:36 +0430

    video

--- app/Http/Controllers/StreamingController.php
+++ app/Http/Controllers/StreamingController.php
@@ -35,54 +35,6 @@ class StreamingController extends Controller
     {
         $loc = __DIR__ . '/../../../../assets/video';
 
-//        $thumbnail_path = '/var/www/assets/videoPicThumnails';
-//        $second             = 1;
-//        $thumbSize       = '150x150';
-//
-//        $videoname  = '/var/www/assets/video/YouTube.mp4';
-//
-//        $cmd = "/usr/bin/ffmpeg -i " . $videoname . " -deinterlace -an -ss {$second} -t 00:00:01  -s {$thumbSize} -r 1 -y -vcodec mjpeg -f mjpeg {$thumbnail_path} 2>&1";
-//
-//        exec($cmd, $output, $retval);
-//
-//        if ($retval)
-//        {
-//            echo 'error in generating video thumbnail';
-//        }
-//        else
-//        {
-//            echo 'Thumbnail generated successfully';
-//            echo $thumb_path = $thumbnail_path . $videoname . '.jpg';
-//        }
-
-        $ffmpeg = '/usr/bin/ffmpeg';
-
-// the input video file
-        $video  = '/var/www/assets/video/YouTube.mp4';
-
-// where you'll save the image
-        $image  = '/var/www/assets/videoPicThumnails/demo.jpg';
-
-// default time to get the image
-        $second = 1;
-
-// get the duration and a random place within that
-        $cmd = "$ffmpeg -i $video 2>&1";
-        if (preg_match('/Duration: ((\d+):(\d+):(\d+))/s', $cmd, $time)) {
-            $total = ($time[2] * 3600) + ($time[3] * 60) + $time[4];
-            $second = rand(1, ($total - 1));
-        }
-
-// get the screenshot
-//$cmd = "$ffmpeg -i $video -deinterlace -an -ss $second -t 00:00:01 -r 1 -y -vcodec mjpeg -f mjpeg $image 2>&1";
-        $return = `$cmd`;
-
-// done! <img src="http://blog.amnuts.com/wp-includes/images/smilies/icon_wink.gif" alt=";-)" class="wp-smiley">
-        echo 'done!';
-
-
-
-        return;
         $videos = scandir($loc);
         foreach ($videos as $video){
             $vidLoc = $loc . '/' . $video;
@@ -101,9 +53,16 @@ class StreamingController extends Controller
                 if(!is_dir($nloc))
                     mkdir($nloc);
 
-//                rename($loc . '/' . $vidLoc)
+                rename($loc . '/' . $video, $nloc .'/'. $video);
+
+                $vidLoc = $nloc . '/' . $video;
+
+                $ffmpeg = FFMpeg\FFMpeg::create();
+                $video = $ffmpeg->open($vidLoc);
+                $frame = $video->frame(FFMpeg\Coordinate\TimeCode::fromSeconds(42));
+                $frame->save($nloc . '/image.jpg');
+                dd('done');
             }
         }
-//        dd(scandir($loc));
     }
 }
