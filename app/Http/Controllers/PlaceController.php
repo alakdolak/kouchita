7ef7e3778946b7de0f1b74cf9f53c530dcd69794
kiavash <kiavashbc@gmail.com>
2020-11-18 16:21:07 +0330

    debug

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -394,16 +394,6 @@ class PlaceController extends Controller {
         $lngBetween = [$D + $lngDeg, $D - $lngDeg];
 
         $tableNames = ['hotels', 'restaurant', 'amaken', 'majara', 'boomgardies'];
-        $s1Time = microtime(true);
-        foreach ($tableNames as $tableName){
-            $nearbys = DB::select("SELECT acos(" . sin($D) . " * sin(D / 180 * 3.14) + " . cos($D) . " * cos(D / 180 * 3.14) * cos(C / 180 * 3.14 - " . $C . ")) * 6371 as distance, id, name, reviewCount, fullRate, slug, alt, cityId, C, D FROM " . $tableName . " HAVING distance between -1 and " . ConfigModel::first()->radius . " order by distance ASC limit 0, " . $count);
-        }
-        $s2Time = microtime(true);
-        foreach ($tableNames as $tableName){
-            $nearbys = DB::select("SELECT id, `name`, reviewCount, fullRate, slug, alt, cityId, `C`, `D` FROM $tableName WHERE `C` > $latBetween[1] AND `C` < $latBetween[0] AND `D` > $lngBetween[1] AND `D` < $lngBetween[0] limit 0, $count" );
-        }
-        $s3Time = microtime(true);
-        dd($s3Time-$s2Time, $s2Time - $s1Time);
 
         foreach ($tableNames as $tableName){
             $kindPlace = Place::where('tableName', $tableName)->first();
@@ -416,7 +406,6 @@ class PlaceController extends Controller {
 //                                  'activityId' => Activity::whereName('نظر')->first()->id];
 //                    $nearby->rate = getRate($nearby->id, $kindPlace->id)[1];
 //                    $nearby->review = LogModel::where($condition)->count();
-
 //                    $nearby->distance = round($nearby->distance, 2);
                     $nearby->pic = getPlacePic($nearby->id, $kindPlace->id);
                     $nearby->review = $nearby->reviewCount;
