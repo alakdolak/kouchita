49d88266a92a6eaf448161724ce446acd3ceb97d
mohammad ghane <mghaneh1375@yahoo.com>
2020-12-06 18:33:55 +0330

    server

--- app/Http/Middleware/ShareData.php
+++ app/Http/Middleware/ShareData.php
@@ -20,7 +20,7 @@ class ShareData
      */
     public function handle($request, Closure $next)
     {
-        $fileVersions = 120;
+        $fileVersions = 121;
 
         $config = \App\models\ConfigModel::first();
         if(auth()->check()){
